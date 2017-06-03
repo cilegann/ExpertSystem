2cc9ce6185ccb603cffdb4f073631aba5f58b571
Cilegann <sean@qiuyuxuandeMacBook-Pro.local>
2017-06-03 16:42:36 +0800

    no message

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <unordered_set>
 
 using namespace std;
 
 //函數宣告
 int countlines(char*);
 string readline(char*,int);
-
-
-
+struct fact;
+struct statement;
+class tree;
+vector<tree> treeVec;
+vector<fact> factVec;
+vector<statement> statementVec;
 
 //事實結構
 struct fact{
@@ -22,52 +26,121 @@ struct fact{
     fact();
     fact(string conclusion){
         this->conclusion=conclusion;
-        this->state="N";
+        this->state="0";
+        this->prompt="X";
     }
     
-};
-
-struct statement{
-    string conclusion;
-    string souldBeState;
-    bool ifStateEqual;
-    statement();
-    statement(string conclu,string souldbe){
-        this->conclusion=conclu;
-        this->souldBeState=souldbe;
-        this->ifStateEqual=0;
+    bool operator == (const fact& rhs) const{
+        return conclusion == rhs.conclusion;
+    }
+    string getConclusioln(){
+        return conclusion;
     }
 };
-//邏輯小結構，包含描述(rule name)、sug(suggestion)、cOR vector<AND vector<狀態結構>>
+
+//小顆狀態結構
+struct statement{
+    string condition;
+    string shouldBeState;
+    bool ifStateEqual;
+    statement(){}
+    statement(string cond,string shouldbe){
+        this->condition=cond;
+        this->shouldBeState=shouldbe;
+        this->ifStateEqual=0;
+    }
+    bool operator==(const statement& rhs) const{
+        return (condition == rhs.condition)&&(shouldBeState == rhs.shouldBeState);
+    }
+};
+
+void factAdder(string conclu){
+    int exist=-1;
+    for(int i=0;i<factVec.size();i++){
+        if(factVec[i].getConclusioln()==conclu){
+            exist=i;
+        }
+    }
+    
+    if(exist==-1){
+        factVec.push_back(fact(conclu));
+    }
+}
+
+//STATEMENT CHECKER AND ADDER
+statement* statementAdder(string conclusion, string shouldBeState){
+    int exist=-1;
+    statement* toReturn;
+    for(int i=0;i<statementVec.size();i++){
+        if(statementVec[i].condition==conclusion && statementVec[i].shouldBeState==shouldBeState){
+            exist=i;
+            break;
+        }
+    }
+    if(exist!=-1){
+        toReturn=&(statementVec[exist]);
+    }else{
+        statement stateTemp =statement(conclusion,shouldBeState);
+        statementVec.push_back(stateTemp);
+        factAdder(conclusion);
+        toReturn = &stateTemp;
+    }
+    return toReturn;
+}
+
+
+//邏輯結構，包含名稱(rule name)、sug(suggestion)、result statement*、OR vector<AND vector<狀態結構*in statement vector>>
 class tree{
 public:
     string name;
     string suggestion;
-    string conclusion;
+    statement* resultStatement;
     vector<vector<statement* > >  OR;
-    tree();
-    tree(string name,string conclusion){
-        this->name=name;
-        this->conclusion=conclusion;
-    }
     
-    tree(string name,string conclusion,string suggestion){
+    tree(){}
+    tree(string name,string conclusion,string shouldBeState){
         this->name=name;
-        this->conclusion=conclusion;
+        resultStatement = statementAdder(conclusion, shouldBeState);
+    }
+    tree(string name,string conclusion,string shouldBeState,string suggestion){
+        this->name=name;
+        resultStatement = statementAdder(conclusion, shouldBeState);
         this->suggestion=suggestion;
     }
+    
+    bool operator==(const tree& rhs) const{
+        return name == rhs.name;
+    }
+    
+    void orPusher(vector<statement*> andvec){
+        OR.push_back(andvec);
+    }
+    
     void printSug(){
         cout<<"BOT>"<<suggestion;
     }
+    
 };
 
 
-
-set<tree*> treeSet;
-set<fact*> factSet;
-set<statement*> statementSet;
-void treeAdder(string ruleName,string conclution,string suggestion,string ifUncut){
-    tree* t=new tree(ruleName,conclution,suggestion);
+//TREE ADDER
+//[REVIEWED]
+void treeAdder(string ruleName,string conclusion,string conclusionShouldBeState,string suggestion,string ifUncut){
+    tree* t;
+    int exist=-1;
+    for(int i=0;i<treeVec.size();i++){
+        if(treeVec[i].name==ruleName){
+            exist=i;
+            break;
+        }
+    }
+    if(exist!=-1){
+        t=(&treeVec[exist]);
+    }else{
+        t =new tree(ruleName,conclusion,conclusionShouldBeState,suggestion);
+        treeVec.push_back(*t);
+    }
+    
     vector<statement*> andVec;
     int numOfCondition=1;
     for(int i=0;i<ifUncut.length();i++){
@@ -75,27 +148,45 @@ void treeAdder(string ruleName,string conclution,string suggestion,string ifUncu
             numOfCondition++;
         }
     }
-    string conditionUncut;
-    string condition;
-    string shouldBecondition;
-    if(numOfCondition==1){
-        
-    }else{
-        
+    int conditionStartIndex=3;
+    int conditionEndIndex;
+    int equalIndex;
+    int shouBeIndex;
+    int andIndex=1;
+    string conditionUncut; //IF is_mammal = T & is_carnivore = T
+    string condition;  //is_mammal
+    string shouldBe;  //T
+    equalIndex = (int)ifUncut.find('=');
+    conditionEndIndex = equalIndex-1;
+    condition = ifUncut.substr(conditionStartIndex,conditionEndIndex - conditionStartIndex);
+    shouBeIndex = equalIndex+2;
+    shouldBe = ifUncut.substr(shouBeIndex,1);
+    statement* s = statementAdder(condition, shouldBe);
+    andVec.push_back(s);
+    if(numOfCondition > 1){
+        for(int i = 1;i < numOfCondition;i++){
+            string conditionUncut; //IF is_mammal = T & is_carnivore = T
+            string condition;  //is_mammal
+            string shouldBe;  //T
+            andIndex = (int)ifUncut.find('&',andIndex+1);
+            equalIndex = (int)ifUncut.find('=',equalIndex+1);
+            conditionEndIndex = equalIndex-1;
+            conditionStartIndex=andIndex+2;
+            condition = ifUncut.substr(conditionStartIndex,conditionEndIndex - conditionStartIndex);
+            shouBeIndex = equalIndex+2;
+            shouldBe = ifUncut.substr(shouBeIndex,1);
+            //statement* s2 = statementAdder(condition, shouldBe);
+            andVec.push_back(statementAdder(condition, shouldBe));
+        }
     }
-    statement* state=new statement();
-    
-    
+    t->orPusher(andVec);
 }
 
 
 
-void factAdder(string conclu){
-    fact* f=new fact(conclu);
-    factSet.insert(f);
-}
 
-//openRule
+
+
 void openrule(string filepath){
     char* filepathChar=&filepath[0u];
     int totalLine=countlines(filepathChar);
@@ -122,8 +213,9 @@ void openrule(string filepath){
             }else{
                 shouldBestate=0;
             }
+            
             factAdder(conclusion);
-            treeAdder(ruleName, conclusion, suggestion,ifStringUncut);
+            treeAdder(ruleName, conclusion, shouldBestateString,suggestion,ifStringUncut);
             
         }
     }
@@ -133,38 +225,6 @@ void openrule(string filepath){
 
 int main(){
     openrule("/Users/sean/Desktop/dp/A_rules.txt");
-    /*
-    string command,parameter;
-    
-    cout<<"OPENRULE/LISTRULE/ADDRULE/OPENCLAUSE/LISTCLAUSE/ADDCLAUSE/DEBUG/QUIT : ";
-    cin>>command;
-    
-
-    while(command!="QUIT"){
-        if(command=="OPENRULE"){
-            cin>>parameter;
-        //呼叫openrule function
-            openrule(parameter);
-        }else if(command=="LISTRULE"){
-        //呼叫列印規則函式
-        }else if(command=="ADDRULE"){
-        //new sub結構
-        }else if(command=="OPENCLAUSE"){
-        //呼叫讀事實函式（傳入事實結構）
-        }else if(command=="LISTCLAUSE"){
-        //呼叫列印事實結構
-        }else if(command=="ADDCLAUSE"){
-        //new sub事實結構
-        }else if(command=="DEBUG"){
-        //??????????
-        }
-        else{
-            cout<<"Command Not Found."<<endl;
-        }
-        cout<<"OPENRULE/LISTRULE/ADDRULE/OPENFACT/LISTCLAUSE/ADDCLAUSE/DEBUG/QUIT";
-        cin>>command;
-    }
-     */
     return 0;
 }
 
@@ -225,3 +285,14 @@ string readline(char* file,int target_line){
 //               佛祖保佑         永無bug
 //
 //***************************************************
+//
+//   █████▒█    ██  ▄████▄   ██ ▄█▀       ██████╗ ██╗   ██╗ ██████╗
+// ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒        ██╔══██╗██║   ██║██╔════╝
+// ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░        ██████╔╝██║   ██║██║  ███╗
+// ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄        ██╔══██╗██║   ██║██║   ██║
+// ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄       ██████╔╝╚██████╔╝╚██████╔╝
+//  ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒       ╚═════╝  ╚═════╝  ╚═════╝
+//  ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+//  ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+//           ░     ░ ░      ░  ░
+//                 ░
